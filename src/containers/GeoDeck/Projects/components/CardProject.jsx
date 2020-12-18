e5300b5d81cb5472e45a8e69cfa732031d60301b
farshad_nejati <nejati.farshad93@gmail.com>
2020-12-18 16:24:08 +0330

    create card project component
    image thumbnail aspect ratio : 16:9

---
+++ src/containers/GeoDeck/Projects/components/CardProject.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Card, CardBody, Col, Button, CardHeader } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+
+const defaultImage = `${process.env.PUBLIC_URL}/img/co2.png`;
+const imageUrl = `${process.env.PUBLIC_URL}/img/gallery/`;
+
+const CardProject = ({ image, title, time, href }) => (
+  <Col md={6} xl={3} sm={12} className="d-flex project-card">
+    <Card>
+      <CardBody className="project-card__container">
+        <CardHeader>
+          <img
+            className={
+              image ? "project-card__img project-card__img--valid" : " project-card__img project-card__img--default"
+            }
+            src={image ? `${imageUrl}${image}` : defaultImage}
+            alt=""
+            style={{
+            //   width: "100%",
+            //    height: "auto"
+            }}
+          />
+        </CardHeader>
+        <div className="project-card__body">
+          {/* <h3 className="project-card__plan">Toy Example</h3>
+          <hr /> */}
+          <p className="project-card__title">{title}</p>
+          <p className="project-card__edited">{`Edited: ${time}`}</p>
+          {/* <p className="project-card__feature">Modified: 1 Dec</p> */}
+          {/* <NavLink
+            className="account__btn btn btn-primary"
+            to="/main"
+            style={{ marginTop: "15px" }}
+          >
+            Load
+          </NavLink> */}
+          <Link to={`/main/${href}`} className="stretched-link" />
+        </div>
+      </CardBody>
+    </Card>
+  </Col>
+);
+
+export default CardProject;
+
+{
+  /*
+  <p className="project-card__feature project-card__feature--inactive">Monthly update</p>
+  <p className="project-card__feature project-card__feature--inactive">Free support</p>
+  */
+}
