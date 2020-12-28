13609923819b35868fa8fb44650cd257b0020993
farshad_nejati <nejati.farshad93@gmail.com>
2020-12-28 23:41:43 +0330

    start creating simTables

--- src/containers/GeoDeck/SimulationView/components/SimTable.jsx
+++ src/containers/GeoDeck/SimulationView/components/SimTable.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { Table } from "reactstrap";
 
-const SimTable = ({simulation}) => {
+const SimTable = ({ simulation }) => {
   console.log(simulation);
+  const { parameters } = simulation;
+  // { label, value, unit, max, min, editable, other }
   return (
-    <div>
-      <p>Sim Table</p>
+    <div className="simulation__table">
+      <div className="simulation__table__head">
+        <div>Variables</div>
+        <div>Current Values</div>
+        <div>Uint</div>
+        <div>Max</div>
+        <div>Min</div>
+        <div>Editable</div>
+        <div>etc.</div>
+      </div>
+      {parameters.map(
+        ({ label, value, unit, max, min, editable, other }, index) => {
+          return (
+            <div className="simulation__table__row" key={index}>
+              <td>{label}</td>
+              <td>{value}</td>
+              <td>{unit}</td>
+              <td>{max}</td>
+              <td>{min}</td>
+              <td>{editable}</td>
+              <td>{other}</td>
+            </div>
+          );
+        }
+      )}
     </div>
   );
 };
