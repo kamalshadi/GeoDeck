2fd2bbc5281a552184d67a60e0b9b3a6492815a2
farshad_nejati <nejati.farshad93@gmail.com>
2021-01-07 16:32:35 +0330

    remove useless code in SimulationChart

--- src/containers/GeoDeck/SimulationChart/index.jsx
+++ src/containers/GeoDeck/SimulationChart/index.jsx
@@ -1,35 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import { Col, Container, Row } from "reactstrap";
 import PlotList from "./components/PlotList";
 import SimulationPanel from "./components/SimulationPanel";
-import { fetchPlots } from "../../../redux/actions/plotAction";
 
 const SimulationChart = (props) => {
-  const { controlBar, simulations } = props;
-  // const [sims, setSims] = useState([]);
-  // const [currentData, setCurrentData] = useState([]);
-
-  // useEffect(() => {
-  //   props.fetchPlots();
-  // }, []);
-
-  // useEffect(() => {
-  //   setSims(simulations);
-  // }, [simulations]);
-
-  // const onSetCurrentData = (id, dataKey) => {
-  //   console.log(`dataKey is: ${dataKey}`);
-  //   console.log(`id is: ${id}`);
-  //   const selectedSim = sims.find((sim) => sim.id === id);
-  //   const data = selectedSim.data[dataKey];
-  //   console.log(data);
-  //   console.log(sims);
-  //   setCurrentData(data);
-  // };
-
-  // console.log(sims);
-
   return (
     <Container className="simulation">
       <Row className="simulation__container">
@@ -39,17 +13,11 @@ const SimulationChart = (props) => {
           style={{ maxHeight: "calc(100vh - 115px)" }}
           // style={{ maxHeight: controlBar ? "calc(100vh - 115px)" : "66vh" }}
         >
-          <SimulationPanel
-          // simulations={sims}
-          // setCurrentData={onSetCurrentData}
-          // currentData={currentData}
-          />
+          <SimulationPanel />
         </Col>
 
         <Col md="10" className="simulation__plot">
-          <PlotList
-          // data={currentData}
-          />
+          <PlotList />
         </Col>
       </Row>
     </Container>
@@ -57,8 +25,3 @@ const SimulationChart = (props) => {
 };
 
 export default SimulationChart;
-// const mapStateToProps = (state) => {
-//   return { simulations: Object.values(state.plots?.data) };
-// };
-
-// export default connect(mapStateToProps, { fetchPlots })(SimulationChart);
