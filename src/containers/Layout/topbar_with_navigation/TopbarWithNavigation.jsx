433226aba2db73ca46314d01217e2e9e5acd2c83
farshad_nejati <nejati.farshad93@gmail.com>
2021-01-14 13:21:09 +0330

    fix topbar links in main

--- src/containers/Layout/topbar_with_navigation/TopbarWithNavigation.jsx
+++ src/containers/Layout/topbar_with_navigation/TopbarWithNavigation.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import TopbarSidebarButton from './TopbarSidebarButton';
-import TopbarProfile from './TopbarProfile';
-import TopbarMail from './TopbarMail';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import TopbarSidebarButton from "./TopbarSidebarButton";
+import TopbarProfile from "./TopbarProfile";
+import TopbarMail from "./TopbarMail";
 // import TopbarLanguage from './TopbarLanguage';
-import TopbarNotification from './TopbarNotification';
-import TopbarNav from './tobar_nav/TopbarNav';
-import TopbarSearch from './TopbarSearch';
+import TopbarNotification from "./TopbarNotification";
+import TopbarNav from "./tobar_nav/TopbarNav";
+import TopbarSearch from "./TopbarSearch";
 
 const TopbarWithNavigation = ({ changeMobileSidebarVisibility }) => (
   <div className="topbar topbar--navigation">
     <div className="topbar__left">
-      <TopbarSidebarButton changeMobileSidebarVisibility={changeMobileSidebarVisibility} />
-      <Link className="topbar__logo" to="/projects" />
+      <TopbarSidebarButton
+        changeMobileSidebarVisibility={changeMobileSidebarVisibility}
+      />
+      <Link className="topbar__logo" to="/" />
+      <TopbarNav />
     </div>
-    <TopbarNav />
     <div className="topbar__right">
       {/* <div className="topbar__right-search">
         <TopbarSearch />
@@ -29,7 +31,6 @@ const TopbarWithNavigation = ({ changeMobileSidebarVisibility }) => (
         {/*
           <TopbarLanguage />
           */}
-
       </div>
     </div>
   </div>
